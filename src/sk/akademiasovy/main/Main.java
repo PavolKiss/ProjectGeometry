e8e69d1c0147b8eda87ee0766572ad70def9f91d
PavolKiss <32639696+PavolKiss@users.noreply.github.com>
2018-01-12 12:57:06 +0100

    added interface

--- src/sk/akademiasovy/main/Main.java
+++ src/sk/akademiasovy/main/Main.java
@@ -1,27 +1,38 @@
 package sk.akademiasovy.main;
 
-import sk.akademiasovy.geometry.Rectangle;
-import sk.akademiasovy.geometry.Square;
+import sk.akademiasovy.geometry2D.Square;
+import sk.akademiasovy.geometry2D.Rectangle;
+import sk.akademiasovy.geometry2D.Triangle;
+import sk.akademiasovy.geometry2D.Circle;
 
-public class Main {
-
-    public static void main(String[] args) {
-        Square square1 = new Square(7.5); //premennej square 1 vytvorim pamatove miesto - (new)
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Square square1 = new Square(7.5);
         Square square2 = new Square();
 
-        System.out.println("Area of square 1 is: "+square1.getArea());
-        System.out.println("Perimeter: "+square1.getPerimeter());
-        System.out.println("Diagonal: "+square1.getDiagonal());
+        System.out.println("Area of square1 is: "+square1.getArea());
+        System.out.println("Perimeter of square1 is: "+square1.getPerimeter());
+        System.out.println("Diagonal of square1 is: "+square1.getDiagonal());
+
+        Rectangle rectangle1 = new Rectangle(7.5,8.5);
+
+        System.out.println("Area of rectangle is: "+rectangle1.getArea());
+        System.out.println("Perimeter of rectangle is: "+rectangle1.getPerimeter());
+
+        Triangle triangle1 = new Triangle();
+        System.out.println("Area of triangle: "+triangle1.getArea());
+        System.out.println("Perimeter of triangle: "+triangle1.getPerimeter());
+        System.out.println("Is Triangle RectAngle: "+triangle1.isRectangular());
+        System.out.println("Is Triangle Equilateral: "+triangle1.isEquilateralTriangle());
+        System.out.println("Is Triangle Isosceles: "+triangle1.isIsoscelesTriangle());
+        System.out.println("Is triangle exist: "+triangle1.existTriangle());
+
+        Circle circle1 = new Circle(4);
+        System.out.println("Area of circle is: "+circle1.getArea());
 
 
-        Rectangle rectangle1 = new Rectangle(18.5,11.15);
-        System.out.println("Rectangle area:"+rectangle1.getAreaofRectangle();
+
     }
-
-
-
-
-
-
-
 }
